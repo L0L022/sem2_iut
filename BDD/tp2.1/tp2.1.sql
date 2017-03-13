5b0449069ae3e8bf0d55e8aadd1cfac3f3969c3b
L0L022 <L0L022@openmailbox.org>
2017-03-13 11:59:46 +0100

    bdd fini tp 2.1

--- BDD/tp2.1/tp2.1.sql
+++ BDD/tp2.1/tp2.1.sql
@@ -11,4 +11,94 @@ ADD COLUMN nbenf NUMERIC(2,0);
 -- 2
 
 ALTER TABLE TP2_bd_voyage.optionv
-ALTER COLUMN libelle TYPE VARCHAR (30);
+ALTER COLUMN libelle TYPE VARCHAR(30);
+
+-- 3 1
+
+CREATE DOMAIN DOM_cat_cli AS VARCHAR(15)
+CONSTRAINT C_cat_cli CHECK (VALUE IN ('PRIVILEGIE', 'BON', 'MAUVAIS'));
+
+ALTER TABLE TP2_bd_voyage.client
+ALTER COLUMN categorie TYPE DOM_cat_cli;
+
+-- 3 2
+
+ALTER TABLE TP2_bd_voyage.voyage
+ADD CONSTRAINT C_nbetoiles CHECK (nbetoiles BETWEEN 1 AND 5);
+
+-- 4
+
+CREATE TABLE tp2_bd_voyage.hotel (
+  id_hotel SERIAL,
+  NomHotel VARCHAR(20) NULL,
+  NbEtoile NUMERIC(1,0) NULL,
+  VilleHotel VARCHAR(20) NULL,
+  CONSTRAINT PK_HOTEL PRIMARY KEY(id_hotel),
+  CONSTRAINT C_nbetoiles CHECK (NbEtoile BETWEEN 1 AND 5)
+);
+
+CREATE TYPE TypeChambre AS ENUM ('Single', 'Double', 'Double luxe', 'Suite', 'Suite junior', 'Suite prestige');
+
+CREATE TABLE tp2_bd_voyage.capacite (
+  id_hotel INTEGER,
+  typeChambre TypeChambre,
+  NbrChambre INTEGER DEFAULT 0,
+  CONSTRAINT PK_CAPACITE PRIMARY KEY(id_hotel, typeChambre)
+);
+
+INSERT INTO tp2_bd_voyage.hotel(NomHotel, NbEtoile, VilleHotel) (
+  SELECT DISTINCT hotel, nbetoiles, villearr
+  FROM tp2_bd_voyage.voyage
+);
+
+ALTER TABLE tp2_bd_voyage.voyage
+ADD COLUMN id_hotel INTEGER;
+
+ALTER TABLE tp2_bd_voyage.voyage
+ADD CONSTRAINT C_id_hotel FOREIGN KEY(id_hotel) REFERENCES tp2_bd_voyage.hotel(id_hotel);
+
+UPDATE tp2_bd_voyage.voyage AS V
+SET id_hotel = (
+  SELECT id_hotel
+  FROM tp2_bd_voyage.hotel
+  WHERE NomHotel = V.HOTEL AND NbEtoile = V.nbetoiles AND VilleHotel = V.villearr
+);
+
+ALTER TABLE tp2_bd_voyage.voyage
+DROP COLUMN villearr;
+
+ALTER TABLE tp2_bd_voyage.voyage
+DROP COLUMN hotel;
+
+ALTER TABLE tp2_bd_voyage.voyage
+DROP COLUMN nbetoiles;
+
+INSERT INTO tp2_bd_voyage.capacite VALUES ((SELECT id_hotel FROM tp2_bd_voyage.hotel WHERE NomHotel = 'ANTIQUE'), 'Single', 10);
+INSERT INTO tp2_bd_voyage.capacite VALUES ((SELECT id_hotel FROM tp2_bd_voyage.hotel WHERE NomHotel = 'ANTIQUE'), 'Double', 75);
+INSERT INTO tp2_bd_voyage.capacite VALUES ((SELECT id_hotel FROM tp2_bd_voyage.hotel WHERE NomHotel = 'ANTIQUE'), 'Suite', 5);
+INSERT INTO tp2_bd_voyage.capacite VALUES ((SELECT id_hotel FROM tp2_bd_voyage.hotel WHERE NomHotel = 'ATLAS'), 'Double', 83);
+INSERT INTO tp2_bd_voyage.capacite VALUES ((SELECT id_hotel FROM tp2_bd_voyage.hotel WHERE NomHotel = 'ATLAS'), 'Suite', 27);
+INSERT INTO tp2_bd_voyage.capacite VALUES ((SELECT id_hotel FROM tp2_bd_voyage.hotel WHERE NomHotel = 'OLD BRIDGE'), 'Single', 25);
+INSERT INTO tp2_bd_voyage.capacite VALUES ((SELECT id_hotel FROM tp2_bd_voyage.hotel WHERE NomHotel = 'OLD BRIDGE'), 'Double', 75);
+INSERT INTO tp2_bd_voyage.capacite VALUES ((SELECT id_hotel FROM tp2_bd_voyage.hotel WHERE NomHotel = 'SAFARI JAMBO'), 'Single', 32);
+INSERT INTO tp2_bd_voyage.capacite VALUES ((SELECT id_hotel FROM tp2_bd_voyage.hotel WHERE NomHotel = 'SAFARI JAMBO'), 'Double', 100);
+INSERT INTO tp2_bd_voyage.capacite VALUES ((SELECT id_hotel FROM tp2_bd_voyage.hotel WHERE NomHotel = 'TRANSATLANTIQUE'), 'Double', 200);
+INSERT INTO tp2_bd_voyage.capacite VALUES ((SELECT id_hotel FROM tp2_bd_voyage.hotel WHERE NomHotel = 'BAMBURI'), 'Double', 150);
+INSERT INTO tp2_bd_voyage.capacite VALUES ((SELECT id_hotel FROM tp2_bd_voyage.hotel WHERE NomHotel = 'BELLERIVE'), 'Double', 56);
+INSERT INTO tp2_bd_voyage.capacite VALUES ((SELECT id_hotel FROM tp2_bd_voyage.hotel WHERE NomHotel = 'BELLERIVE'), 'Suite', 12);
+INSERT INTO tp2_bd_voyage.capacite VALUES ((SELECT id_hotel FROM tp2_bd_voyage.hotel WHERE NomHotel = 'DAR AL BAHAR'), 'Single', 10);
+INSERT INTO tp2_bd_voyage.capacite VALUES ((SELECT id_hotel FROM tp2_bd_voyage.hotel WHERE NomHotel = 'DAR AL BAHAR'), 'Double', 12);
+INSERT INTO tp2_bd_voyage.capacite VALUES ((SELECT id_hotel FROM tp2_bd_voyage.hotel WHERE NomHotel = 'EL ALMARANTE'), 'Single', 34);
+INSERT INTO tp2_bd_voyage.capacite VALUES ((SELECT id_hotel FROM tp2_bd_voyage.hotel WHERE NomHotel = 'EL ALMARANTE'), 'Suite', 45);
+INSERT INTO tp2_bd_voyage.capacite VALUES ((SELECT id_hotel FROM tp2_bd_voyage.hotel WHERE NomHotel = 'EL ANDALOUS'), 'Single', 67);
+INSERT INTO tp2_bd_voyage.capacite VALUES ((SELECT id_hotel FROM tp2_bd_voyage.hotel WHERE NomHotel = 'EL ANDALOUS'), 'Double', 54);
+INSERT INTO tp2_bd_voyage.capacite VALUES ((SELECT id_hotel FROM tp2_bd_voyage.hotel WHERE NomHotel = 'ELIAS BEACH'), 'Single', 87);
+INSERT INTO tp2_bd_voyage.capacite VALUES ((SELECT id_hotel FROM tp2_bd_voyage.hotel WHERE NomHotel = 'ELIAS BEACH'), 'Double', 33);
+INSERT INTO tp2_bd_voyage.capacite VALUES ((SELECT id_hotel FROM tp2_bd_voyage.hotel WHERE NomHotel = 'ESPADON'), 'Single', 34);
+INSERT INTO tp2_bd_voyage.capacite VALUES ((SELECT id_hotel FROM tp2_bd_voyage.hotel WHERE NomHotel = 'ESPADON'), 'Double', 45);
+INSERT INTO tp2_bd_voyage.capacite VALUES ((SELECT id_hotel FROM tp2_bd_voyage.hotel WHERE NomHotel = 'HASNA'), 'Single', 32);
+INSERT INTO tp2_bd_voyage.capacite VALUES ((SELECT id_hotel FROM tp2_bd_voyage.hotel WHERE NomHotel = 'HASNA'), 'Double', 24);
+INSERT INTO tp2_bd_voyage.capacite VALUES ((SELECT id_hotel FROM tp2_bd_voyage.hotel WHERE NomHotel = 'MONDIAL'), 'Single', 12);
+INSERT INTO tp2_bd_voyage.capacite VALUES ((SELECT id_hotel FROM tp2_bd_voyage.hotel WHERE NomHotel = 'MONDIAL'), 'Double', 13);
+INSERT INTO tp2_bd_voyage.capacite VALUES ((SELECT id_hotel FROM tp2_bd_voyage.hotel WHERE NomHotel = 'KENZI CLUB'), 'Single', 45);
+INSERT INTO tp2_bd_voyage.capacite VALUES ((SELECT id_hotel FROM tp2_bd_voyage.hotel WHERE NomHotel = 'KENZI CLUB'), 'Double', 67);
