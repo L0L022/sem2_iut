a161145d9b21cd9b9cb520dcc7e0628c19b611d0
L0L022 <L0L022@openmailbox.org>
2017-05-19 23:50:44 +0200

    super formes

--- cpp/ez-tests/move_point.cpp
+++ cpp/ez-tests/move_point.cpp
@@ -88,8 +88,16 @@ public:
     m_window.fillCircle(topRight.x(), topRight.y(), bottomLeft.x(), bottomLeft.y());
   }
 
-  void 	drawText(const EZAlign align, const Point &pos, const std::string &text) {
+  void drawText(const EZAlign align, const Point &pos, const std::string &text) {
     m_window.drawText(align, pos.x(), pos.y(), text);
+  }
+
+  void drawLine(const Point &first, const Point &second) {
+    m_window.drawLine(first.x(), first.y(), second.x(), second.y());
+  }
+
+  void drawRectangle(const Point &topRight, const Point &bottomLeft) {
+    m_window.drawRectangle(topRight.x(), topRight.y(), bottomLeft.x(), bottomLeft.y());
   }
 
 private:
@@ -100,16 +108,24 @@ class GraphicsItem {
 public:
   typedef std::vector<GraphicsItem*> GraphicsItemList;
 
-  enum Types {
+  enum GraphicsTypes { //mouse movable type ?
     UndefinedType = 0x0,
     ItemType = 0x1,
     PointType = 0x2,
     TextType = 0x4,
-    ShapeType = 0x6
+    ShapeType = 0x6,
+    LineType = 0x8,
+    TriangleType = 0x10,
+    RectangleType = 0x12
+  };
+
+  enum SearchTypes {
+    DirectChildren,
+    ChildrenRecursively
   };
 
   explicit GraphicsItem(GraphicsItem *parent = nullptr)
-  : m_parent(nullptr), m_children(), m_anchor(), m_color(ez_black), m_thick(1)
+  : m_parent(nullptr), m_children(), m_anchor(), m_color(ez_black), m_thick(1), m_isVisible(true)
   {
     setParent(parent);
   }
@@ -140,15 +156,20 @@ public:
     }
   }
 
-  virtual Types type() const {
+  virtual GraphicsTypes type() const {
     return ItemType;
   }
 
-  GraphicsItemList children(const Types filter = UndefinedType) const {
+  GraphicsItemList children(const GraphicsTypes filter = UndefinedType, const SearchTypes option = ChildrenRecursively) const {
     GraphicsItemList list;
-    for (auto &ptr : m_children)
+    for (auto &ptr : m_children) {
+      if (option == ChildrenRecursively) {
+        GraphicsItemList sublist = ptr->children(filter, option);
+        list.insert(list.end(), sublist.begin(), sublist.end());
+      }
       if (filter == UndefinedType or ptr->type() == filter)
         list.push_back(ptr.get());
+    }
     return list;
   }
 
@@ -179,7 +200,18 @@ public:
     m_thick = thick;
   }
 
+  bool isVisible() const {
+    return m_isVisible;
+  }
+
+  void setIsVisible(const bool isVisible = true) {
+    m_isVisible = isVisible;
+  }
+
   void draw(Canvas *canvas) {
+    if (!m_isVisible)
+      return;
+
     for (auto &ptr : m_children)
       ptr->draw(canvas);
     if(canvas) {
@@ -190,6 +222,9 @@ public:
   }
 
   bool isOver(const Point &p) {
+    if (!m_isVisible)
+      return false;
+
     bool result = false;
     for (auto &ptr : m_children)
       result |= ptr->isOver(p);
@@ -206,6 +241,7 @@ private:
   Point m_anchor;
   EZColor m_color;
   unsigned int m_thick;
+  bool m_isVisible;
 };
 
 class GraphicsPoint : public GraphicsItem
@@ -232,7 +268,7 @@ public:
     m_radius = radius;
   }
 
-  Types type() const {
+  GraphicsTypes type() const {
     return PointType;
   }
 
@@ -276,7 +312,7 @@ public:
     m_align = align;
   }
 
-  Types type() const {
+  GraphicsTypes type() const {
     return TextType;
   }
 
@@ -297,14 +333,115 @@ private:
   EZAlign m_align;
 };
 
+class GraphicsRectangle : public GraphicsItem
+{
+public:
+  explicit GraphicsRectangle(GraphicsItem *parent = nullptr)
+  : GraphicsItem(parent), m_topLeft(nullptr), m_bottomRight(nullptr)
+  {
+    m_topLeft = new GraphicsPoint(this);
+    m_bottomRight = new GraphicsPoint(this);
+  }
+
+  GraphicsItem *topLeft() {
+    return m_topLeft;
+  }
+
+  GraphicsItem *bottomRight() {
+    return m_bottomRight;
+  }
+
+  GraphicsTypes type() const {
+    return RectangleType;
+  }
+
+protected:
+  void meDraw(Canvas *canvas)
+  {
+    if (canvas)
+      canvas->drawRectangle(topLeft()->anchor(), bottomRight()->anchor());
+  }
+
+  bool meIsOver(const Point &p)
+  {
+    return topLeft()->anchor() < p and p < bottomRight()->anchor();
+  }
+
+private:
+  unsigned int m_radius;
+  GraphicsItem *m_topLeft, *m_bottomRight;
+};
+
 class GraphicsShape : public GraphicsItem {
 public:
-  explicit GraphicsShape(GraphicsItem *parent = nullptr) : GraphicsItem(parent) {}
+  explicit GraphicsShape(GraphicsItem *parent = nullptr) : GraphicsItem(parent), m_points() {}
 
-  Types type() const {
+  GraphicsTypes type() const {
     return ShapeType;
   }
-private:
+
+protected:
+  void meDraw(Canvas *canvas)
+  {
+    if (canvas)
+      for (size_t i = 0; i < m_points.size(); ++i)
+        canvas->drawLine(m_points[i]->anchor(), m_points[(i+1)%m_points.size()]->anchor());
+  }
+
+protected:
+  GraphicsItemList m_points;
+};
+
+class GraphicsLine : public GraphicsShape {
+public:
+  explicit GraphicsLine(GraphicsItem *parent = nullptr) : GraphicsShape(parent)
+  {
+    GraphicsPoint *first = new GraphicsPoint(this);
+    GraphicsPoint *second = new GraphicsPoint(this);
+    m_points.push_back(first);
+    m_points.push_back(second);
+  }
+
+  GraphicsItem *first() {
+    return m_points[0];
+  }
+
+  GraphicsItem *second() {
+    return m_points[1];
+  }
+
+  GraphicsTypes type() const {
+    return LineType;
+  }
+};
+
+class GraphicsTriangle : public GraphicsShape {
+public:
+  explicit GraphicsTriangle(GraphicsItem *parent = nullptr) : GraphicsShape(parent)
+  {
+    GraphicsPoint *first = new GraphicsPoint(this);
+    GraphicsPoint *second = new GraphicsPoint(this);
+    GraphicsPoint *third = new GraphicsPoint(this);
+    m_points.push_back(first);
+    m_points.push_back(second);
+    m_points.push_back(third);
+  }
+
+  GraphicsItem *first() {
+    return m_points[0];
+  }
+
+  GraphicsItem *second() {
+    return m_points[1];
+  }
+
+  GraphicsItem *third() {
+    return m_points[2];
+  }
+
+  GraphicsTypes type() const {
+    return TriangleType;
+  }
 };
 
 class GraphicsCircle : public GraphicsShape {
@@ -321,11 +458,31 @@ public:
     GraphicsText *text1 = new GraphicsText("EZDraw c'est rigolo ^^", point);
     text1->setAnchor(Point(40, 30));
     text1->setColor(ez_green);
+    text1->setIsVisible(false);
+
+    GraphicsPoint *point2 = new GraphicsPoint(&m_scene);
+    point2->setColor(ez_yellow);
+    GraphicsText *text2 = new GraphicsText("Bonjour les gens !!!!", point2);
+    text2->setColor(ez_red);
+
+    GraphicsLine *line = new GraphicsLine(&m_scene);
+    line->second()->setAnchor(Point(40, 0));
+
+    GraphicsTriangle *triangle = new GraphicsTriangle(&m_scene);
+    triangle->first()->setAnchor(Point(50, 10));
+    triangle->second()->setAnchor(Point(40, 20));
+    triangle->third()->setAnchor(Point(60, 20));
+
+    GraphicsRectangle *rectangle = new GraphicsRectangle(&m_scene);
+    rectangle->topLeft()->setAnchor(Point(70, 70));
+    rectangle->bottomRight()->setAnchor(Point(90, 90));
   }
 
   void buttonPress(int x, int y, int button) {
-    if (m_scene.isOver(Point(x, y)))
-      currentItem = &m_scene;
+    for (GraphicsItem *item : m_scene.children(GraphicsItem::PointType)) {
+      if (item->isOver(Point(x, y)))
+        currentItem = item;
+    }
   }
 
   void buttonRelease(int x, int y, int button) {
